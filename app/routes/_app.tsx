dafc75eae25f6956ae3570ce92fa9f0533474048
Rob Martin <rmartin3@alumni.unca.edu>
2024-02-20 17:39:26 -0500

    client loader for tabs

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "@remix-run/react";
 import Topbar from "~/components/topbar";
 import Sidebar from "~/components/sidebar";
-import { fetchOptions } from "~/utils/tab.storage";
+import { fetchTabs } from "~/utils/tab.storage";
 
 export const clientLoader = async () => {
-  const options = fetchOptions();
-  return { options };
+  const tabs = fetchTabs();
+  return { tabs };
 };
 
 export default function App() {
   return (
     <div className="border-b dark:border-neutral-700">
       <Topbar />
-      <div className="container mx-auto flex">
+      <div className="flex">
         <div className="w-auto border-x dark:border-neutral-700">
           <Sidebar />
         </div>
