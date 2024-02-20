5e504a772ddd32d4121f212639d77efba5f58864
Rob Martin <rmartin3@alumni.unca.edu>
2024-02-20 17:39:17 -0500

    dataset page loader

--- app/routes/_app.datasets._index.tsx
+++ app/routes/_app.datasets._index.tsx
@@ -19,81 +19,94 @@ export default function DatasetsPage() {
   const handleClickViewDataset = (dataset: Partial<Dataset>) => {
     const { id, name } = dataset;
     const formData = new FormData();
-    formData.append("tab", JSON.stringify({ id, name, intent: "create-tab" }));
-
-    createTabFetcher.submit(formData, {
-      method: "post",
-      action: "/",
-      navigate: false,
-    });
+    if (!id || !name) return;
+    formData.append("id", id.toString());
+    formData.append("name", name);
+    formData.append("intent", "create-tab");
+    createTabFetcher.submit(
+      { id, name, intent: "create-tab" },
+      {
+        method: "post",
+        action: "/",
+        navigate: false,
+      }
+    );
 
     navigate(`/datasets/${id}`);
   };
 
   return (
-    <div>
-      <table className="min-w-full divide-y divide-neutral-200">
-        <thead className="sticky top-0 left-0 dark:bg-neutral-800 bg-neutral-100">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
-            >
-              ID
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
-            >
-              Name
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
-            >
-              Created At
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
-            >
-              View
-            </th>
-          </tr>
-        </thead>
-        <tbody className="">
-          {datasets.map((dataset: any) => (
-            <tr
-              key={dataset.id}
-              className="dark:hover:bg-neutral-800 hover:bg-neutral-100 transition-all ease-in-out duration-100"
-            >
-              <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700">
-                {dataset.id}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700">
-                {dataset.name}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700">
-                {new Date(dataset.createdAt).toLocaleString()}
-              </td>
-
-              <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700 cursor-pointer">
-                <div
-                  onClick={() =>
-                    handleClickViewDataset({
-                      name: dataset.name,
-                      id: dataset.id,
-                    })
-                  }
-                  className="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-600 font-semibold"
-                >
-                  View
-                </div>
-              </td>
+    <div className="flex h-[calc(100vh-71px)] w-[calc(100vw-78px)]">
+      <div className="w-full overflow-x-auto">
+        <table className="min-w-full divide-y divide-neutral-200">
+          <thead className="sticky top-0 left-0 dark:bg-neutral-800 bg-neutral-100">
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
+              >
+                ID
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
+              >
+                Name
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
+              >
+                Created At
+              </th>
+              {/* <th
+                scope="col"
+                className="px-6 py-4 text-left text-xs font-medium text-neutral-400 uppercase tracking-wider border-b border-r last:border-r-0 dark:border-neutral-700"
+              >
+                Open
+              </th> */}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="">
+            {datasets.map((dataset: any) => (
+              <tr
+                onClick={() =>
+                  handleClickViewDataset({
+                    name: dataset.name,
+                    id: dataset.id,
+                  })
+                }
+                key={dataset.id}
+                className="dark:hover:bg-neutral-800 hover:bg-neutral-100 cursor-pointer transition-all ease-in-out duration-100"
+              >
+                <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700">
+                  {dataset.id}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700">
+                  {dataset.name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700">
+                  {new Date(dataset.createdAt).toLocaleString()}
+                </td>
+
+                {/* <td className="px-6 py-4 whitespace-nowrap text-md border-r border-b last:border-r-0  dark:border-neutral-700 cursor-pointer">
+                  <div
+                    onClick={() =>
+                      handleClickViewDataset({
+                        name: dataset.name,
+                        id: dataset.id,
+                      })
+                    }
+                    className=" hover:text-indigo-600 dark:hover:text-indigo-400 font-semibold"
+                  >
+                    <ArrowRightIcon height={20} width={20} />
+                  </div>
+                </td> */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
