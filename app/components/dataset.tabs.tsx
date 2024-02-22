9dfde52731c12c0a6a4d6df190bf1f9df6fdf132
Rob Martin <rmartin3@alumni.unca.edu>
2024-02-22 14:27:49 -0500

    cleanup

--- app/components/dataset.tabs.tsx
+++ app/components/dataset.tabs.tsx
@@ -1,5 +1,10 @@
 import { Cross2Icon } from "@radix-ui/react-icons";
-import { useFetcher, Link, useParams, useLoaderData } from "@remix-run/react";
+import {
+  useFetcher,
+  NavLink,
+  useParams,
+  useLoaderData,
+} from "@remix-run/react";
 import { useEffect, useState, useRef } from "react";
 
 type Tab = { value: number; label: string };
@@ -40,8 +45,6 @@ const DatasetTabs = () => {
 
   const handleEditConfirm = (e: any, index: number, tab: Tab) => {
     if (e.key === "Enter") {
-      const updatedTab = { ...tab, label: editingName };
-      updateTab(index, updatedTab);
       setEditingIndex(null);
 
       const formData = new FormData();
@@ -61,8 +64,6 @@ const DatasetTabs = () => {
   };
 
   const handleEditConfirmOnBlur = (index: number, tab: Tab) => {
-    const updatedTab = { ...tab, label: editingName };
-    updateTab(index, updatedTab);
     setEditingIndex(null);
 
     const formData = new FormData();
@@ -80,18 +81,11 @@ const DatasetTabs = () => {
     );
   };
 
-  const updateTab = (index: number, updatedTab: Tab) => {
-    // Implement the logic to update the option in your state or data source
-  };
-
   return (
     <div className="w-full h-full overflow-auto">
       <div className="flex whitespace-nowrap h-full">
         {tabs.map((tab: any, index: number) => (
-          <div
-            key={tab.value}
-            className="flex-none flex items-center border-r dark:border-neutral-700 gap-2 px-2 hover:bg-neutral-100 dark:hover:bg-neutral-800 transition-colors duration-200 ease-in-out"
-          >
+          <>
             {editingIndex === index ? (
               <input
                 ref={(el) => (editInputRefs.current[index] = el)}
@@ -100,34 +94,42 @@ const DatasetTabs = () => {
                 onChange={handleEditChange}
                 onKeyPress={(e) => handleEditConfirm(e, index, tab)}
                 onBlur={() => handleEditConfirmOnBlur(index, tab)}
-                className="text-left p-2 w-full dark:bg-neutral-700 bg-neutral-200 border-none focus:ring-0 focus:outline-none rounded"
+                className="cursor-pointer p-4 flex-none items-center border-r dark:border-neutral-700 px-2 hover:bg-neutral-100 dark:hover:bg-neutral-800 transition-colors duration-200 ease-in-out focus:outline-none focus:bg-blue-50 focus:dark:bg-blue-800"
               />
             ) : (
-              <>
-                <Link
-                  to={`/datasets/${tab.value}`}
-                  className="flex-1 cursor-pointer p-5"
+              <NavLink
+                key={tab.value}
+                to={`/datasets/${tab.value}`}
+                className={({ isActive }) => {
+                  console.log(tab.value, isActive);
+                  const commonStyles =
+                    "cursor-pointer p-4 flex-none flex items-center border-r dark:border-neutral-700 gap-2 px-2 hover:bg-neutral-100 dark:hover:bg-neutral-800 transition-colors duration-200 ease-in-out";
+                  if (isActive) {
+                    return commonStyles + " bg-neutral-100 dark:bg-neutral-700";
+                  } else {
+                    return commonStyles;
+                  }
+                }}
+              >
+                <span
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setEditingIndex(index);
+                    setEditingName(tab.label);
+                  }}
+                  className="cursor-pointer p-1 hover:bg-neutral-300 dark:hover:bg-neutral-900 transition-colors duration-200 ease-in-out rounded"
                 >
-                  <span
-                    onClick={(e) => {
-                      e.preventDefault(); // Prevent link navigation
-                      setEditingIndex(index);
-                      setEditingName(tab.label);
-                    }}
-                    className="cursor-pointer p-1 hover:bg-neutral-300 dark:hover:bg-neutral-900 transition-colors duration-200 ease-in-out rounded"
-                  >
-                    {tab.label}
-                  </span>
-                </Link>
+                  {tab.label}
+                </span>
                 <button
                   onClick={() => handleRemoveTab(tab.value)}
                   className="p-2"
                 >
                   <Cross2Icon />
                 </button>
-              </>
+              </NavLink>
             )}
-          </div>
+          </>
         ))}
       </div>
     </div>
